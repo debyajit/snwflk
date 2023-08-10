66feb628d587310ab94d86cddc21e2750cb138a6
Deb <39284636+debyajit@users.noreply.github.com>
2023-08-10 11:55:40 -0400

    Update gen_random_data.py

--- snwflk_prj1/gen_random_data.py
+++ snwflk_prj1/gen_random_data.py
@@ -1,39 +1,31 @@
-# input file str 
-#fieldname|datatype
-#act_week|date
-
-
-import csv
+mport csv
 import random
-from datetime import date
+from datetime import datetime, timedelta
 
-def generate_csv(input_file, output_file):
-    with open(input_file, 'r') as f:
-        reader = csv.reader(f, delimiter='|')
-        fieldnames = [row[0] for row in reader]
-        datatypes = [row[1] for row in reader]
-
-    with open(output_file, 'w', newline='') as f:
-        writer = csv.writer(f, delimiter='~', quoting=csv.QUOTE_ALL)
-        writer.writerow(fieldnames)
-
-        for _ in range(1000):
+def generate_csv(file_name, field_names):
+    with open(file_name, mode='w', newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter='~', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(field_names)
+        for i in range(1000):
             row = []
-            for datatype in datatypes:
-                if datatype == 'date':
-                    value = str(date.today())
-                elif datatype == 'char':
-                    value = random.choice('abcdefghijklmnopqrstuvwxyz')
-                elif datatype == 'number':
-                    value = str(random.randint(1, 100))
-                elif datatype == 'float':
-                    value = str(random.uniform(1, 100))
-                else:
-                    value = 'Unknown Data Type'
-                row.append(value)
+            for field in field_names:
+                if field.split('|')[1] == 'int':
+                    row.append(random.randint(1, 100))
+                elif field.split('|')[1] == 'float':
+                    row.append(random.uniform(1.0, 100.0))
+                elif field.split('|')[1] == 'str':
+                    row.append(''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', k=10)))
+                elif field.split('|')[1] == 'date':
+                    start_date = datetime.now() - timedelta(days=365)
+                    end_date = datetime.now()
+                    time_between_dates = end_date - start_date
+                    days_between_dates = time_between_dates.days
+                    random_number_of_days = random.randrange(days_between_dates)
+                    random_date = start_date + timedelta(days=random_number_of_days)
+                    row.append(random_date.strftime('%Y-%m-%d'))
             writer.writerow(row)
 
-# Usage example
-input_file = 'input.txt'
-output_file = 'output.csv'
-generate_csv(input_file, output_file)
+if __name__ == '__main__':
+    file_name = 'output.csv'
+    field_names = ['Field1|int', 'Field2|float', 'Field3|str', 'act_week|date']
+    generate_csv(file_name, field_names)
