60fd4cceba6c0c17dadc819c4397e5879d4f227e
Deb <39284636+debyajit@users.noreply.github.com>
2023-09-12 09:16:23 -0400

    Update dq_check.sql

--- snwflk_prj1/dq_check.sql
+++ snwflk_prj1/dq_check.sql
@@ -15,3 +15,42 @@ SELECT
     ) WITHIN GROUP (ORDER BY column_name) ||
     ' FROM your_table_name' AS dynamic_query
 FROM column_names;
+----------------
+
+-- Create a list of columns in the table
+WITH column_names AS (
+    SELECT column_name
+    FROM information_schema.columns
+    WHERE table_name = 'your_table_name' -- Replace with your actual table name
+),
+-- Generate dynamic SQL to calculate statistics for each column
+dynamic_sql AS (
+    SELECT
+        'SELECT ''' || column_name || ''' AS column_name, ' || 
+        'COUNT(' || column_name || ') AS null_count, ' ||
+        'COUNT(DISTINCT ' || column_name || ') AS distinct_count, ' ||
+        'MAX(' || column_name || ') AS max_value, ' ||
+        'MIN(' || column_name || ') AS min_value ' ||
+        'FROM your_table_name' AS dynamic_query
+    FROM column_names
+)
+-- Execute dynamic SQL queries and combine results
+SELECT *
+FROM (
+    SELECT
+        column_name,
+        null_count,
+        distinct_count,
+        max_value,
+        min_value
+    FROM dynamic_sql
+    UNION ALL
+    SELECT
+        'Total' AS column_name,
+        SUM(null_count) AS null_count,
+        SUM(distinct_count) AS distinct_count,
+        NULL AS max_value,
+        NULL AS min_value
+    FROM dynamic_sql
+)
+ORDER BY column_name;
